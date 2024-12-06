617b636861a770a6e1475c46c8a0cd540c246aa8
somaz <genius5711@gmail.com>
2024-12-06 17:22:52 +0900

    chore: fix filter

--- internal/controller/filters.go
+++ internal/controller/filters.go
@@ -1,9 +1,11 @@
 package controller
 
 import (
+	"context"
 	"path/filepath"
 
 	syncv1 "github.com/somaz94/k8s-namespace-sync/api/v1"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 // shouldSyncResource determines if a resource should be synced based on its name and filter rules
@@ -38,27 +40,34 @@ func (r *NamespaceSyncReconciler) shouldSyncResource(name string, filter *syncv1
 
 // shouldSyncToNamespace determines if resources should be synced to the given namespace
 func (r *NamespaceSyncReconciler) shouldSyncToNamespace(namespace string, namespaceSync *syncv1.NamespaceSync) bool {
+	logger := log.FromContext(context.Background()).WithValues("namespace", namespace)
+
 	// 시스템 네임스페이스 체크
 	if r.isSystemNamespace(namespace) {
+		logger.Info("Namespace is system namespace, skipping sync")
 		return false
 	}
 
 	// 소스 네임스페이스와 동일한 경우 스킵
 	if namespace == namespaceSync.Spec.SourceNamespace {
+		logger.Info("Namespace is source namespace, skipping sync")
 		return false
 	}
 
 	// 제외 목록에 있는 경우 스킵
 	if contains(namespaceSync.Spec.Exclude, namespace) {
+		logger.Info("Namespace is in exclude list, skipping sync")
 		return false
 	}
 
 	// targetNamespaces가 지정된 경우, 해당 목록에 있는 네임스페이스만 동기화
 	if len(namespaceSync.Spec.TargetNamespaces) > 0 {
-		return contains(namespaceSync.Spec.TargetNamespaces, namespace)
+		shouldSync := contains(namespaceSync.Spec.TargetNamespaces, namespace)
+		logger.Info("Checking target namespaces", "shouldSync", shouldSync)
+		return shouldSync
 	}
 
-	// targetNamespaces가 지정되지 않은 경우 모든 네임스페이스에 동기화
+	logger.Info("Namespace will be synced")
 	return true
 }
 
@@ -69,6 +78,7 @@ func (r *NamespaceSyncReconciler) isSystemNamespace(namespace string) bool {
 		"kube-public",
 		"kube-node-lease",
 		"default",
+		"k8s-namespace-sync-system",
 	}
 	return contains(systemNamespaces, namespace)
 }
